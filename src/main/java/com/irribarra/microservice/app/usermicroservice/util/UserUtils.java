94899968ab1815dd58ce5e97c85ac460814c6001
Patricio Irribarra <irribarra.naudon@gmail.com>
2020-10-09 01:48:46 -0300

    clase utilitaria para validacion de datos

--- src/main/java/com/irribarra/microservice/app/usermicroservice/util/UserUtils.java
+++ src/main/java/com/irribarra/microservice/app/usermicroservice/util/UserUtils.java
@@ -1,2 +1,12 @@
-package com.irribarra.microservice.app.usermicroservice.util;public class UserUtils {
+package com.irribarra.microservice.app.usermicroservice.util;
+
+public class UserUtils {
+
+    public void validateEmailFormatt(String email) {
+        //// TODO: 09-10-20 logica para validar formato de email 
+    }
+
+    public void validatePasswordFormatt(String password) {
+        // TODO: 09-10-20 lógica para validar formato de password  
+    }
 }
