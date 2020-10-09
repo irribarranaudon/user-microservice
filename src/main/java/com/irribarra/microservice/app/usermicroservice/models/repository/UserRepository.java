053b6816414b7a408a3362a4fca78c1e65067747
Patricio Irribarra <irribarra.naudon@gmail.com>
2020-10-09 01:47:15 -0300

    nuevo metodo dao para la busqueda de usuarios por email para validacion de unique constraint

--- src/main/java/com/irribarra/microservice/app/usermicroservice/models/repository/UserRepository.java
+++ src/main/java/com/irribarra/microservice/app/usermicroservice/models/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.irribarra.microservice.app.usermicroservice.models.repository;
 import com.irribarra.microservice.app.usermicroservice.models.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
 }
