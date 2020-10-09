dc5b9b627c0529b33c86a40263dc0b6149d44dff
Patricio Irribarra <irribarra.naudon@gmail.com>
2020-10-09 01:50:23 -0300

    paquete de clases para mantener informacion util para posterior creacion de entidades persitentes

---
+++ src/main/java/com/irribarra/microservice/app/usermicroservice/models/dto/UserRequestDTO.java
@@ -0,0 +1,7 @@
+package com.irribarra.microservice.app.usermicroservice.models.dto;
+
+public class UserRequestDTO {
+    private String name;
+    private String email;
+    private String password;
+}
